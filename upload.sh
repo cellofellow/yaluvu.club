d89b01ae38e8a28387a5d7c55824ad921a3c8f87
Joshua Gardner <mellowcellofellow@gmail.com>
2014-09-24 01:54:31 -0600

    Enable gzip and use pngcrush on pngs.
    Better score in Google PageSpeed Insights

--- upload.sh
+++ upload.sh
@@ -1,3 +1,29 @@
 #!/bin/bash
 
-s3cmd -c s3conf sync -rr --delete-removed --exclude="*.swp" public/ s3://www.yaluvu.club/
+function gzip_file() {
+    infile="$1"
+    outfile="${1}.gz"
+    gzip -9 $infile
+    mv "$outfile" "$infile"
+
+}
+
+export -f gzip_file
+
+function gunzip_file() {
+    infile="$1"
+    outfile="${1}.gz"
+    mv "$infile" "$outfile"
+    gunzip $outfile
+}
+
+export -f gunzip_file
+
+find public -type f -exec bash -c 'gzip_file "$0"' {} \;
+
+s3cmd -c s3conf sync -rr --delete-removed --exclude="*.swp" \
+    --add-header='Content-Encoding: gzip' \
+    --add-header='Content-Control: max-age=84600' \
+    public/ s3://www.yaluvu.club/
+
+find public -type f -exec bash -c 'gunzip_file "$0"' {} \;
